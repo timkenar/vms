471911f14e6940c8beaa5c6bf582bafff09dcee4
Timothy Chepkener <97870990+timkenar@users.noreply.github.com>
2024-05-09 00:36:27 +0300

    admin.py

--- hosts/admin.py
+++ hosts/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Host, Meeting
 
 # Register your models here
+class ModifyAdmin(admin.ModelAdmin):
+    list_display = ['id','visitor_name','visitor_phone','date','time_in','time_out']
+    search_fields = ['id','visitor_name','visitor_phone', 'location']
+
 admin.site.register(Host)
-#Added a subsection to Modify admin depending on privilledges 
+#Added a subsection to Modify admin depending on priviledges 
 admin.site.register(Meeting, ModifyAdmin)
